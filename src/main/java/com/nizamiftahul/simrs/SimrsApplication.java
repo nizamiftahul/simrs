6a80829d2c9e91f3f7372fa6b2476acc713e2dd9
nizamiftahul <nizamiftahul@gmail.com>
2026-07-24 09:05:47 +0700

    init

---
+++ src/main/java/com/nizamiftahul/simrs/SimrsApplication.java
@@ -0,0 +1,13 @@
+package com.nizamiftahul.simrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nizamiftahul/simrs/SimrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nizamiftahul.simrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
